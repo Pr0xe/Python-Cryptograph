c360b6be8d00f9b42aa0d9fc368a0d3db0006449
Pr0xe <30048366+Pr0xe@users.noreply.github.com>
2019-04-18 18:58:54 +0300

    Add files via upload

---
+++ crypto.py
@@ -0,0 +1,25 @@
+import sys
+def encrypt(text,s):
+    result = ""
+
+    #transverse the plain text
+    for i in range(len(text)):
+        char = text[i]
+
+        #encrypt uppercase characters in plain text
+        if (char.isupper()):
+            result += chr((ord(char)+ s - 65) % 26 + 65)
+
+        #Encrypt lowercase characters in plain text
+        else:
+            result += chr((ord(char)+ s - 97) % 26 + 97)
+    return result
+
+#check the above fuction
+text = str(input('What are you thinking :'))
+s = 5
+print ("Plain Text  : {}" .format(text))
+print ("Shift pattern : {}" .format(str(s)))
+print ("Cipher: {}" .format (encrypt(text,s)))
+
+    
---
+++ hackcrypto.py
@@ -0,0 +1,14 @@
+message = input("Add what you want: ") #encmessage
+LETTERS =  'AaBbCcDdEeFfGgHhIiJjKkLlMmNnOoPpQqRrSsTtUuVvWwXxYyZz'
+for key in range(len(LETTERS)):
+    translated = ''
+    for symbol in message:
+        if symbol in LETTERS:
+            num = LETTERS.find(symbol)
+            num = num - key
+            if num < 0:
+                num = num + len(LETTERS)
+            translated = translated + LETTERS[num]
+        else:
+            translated = translated + symbol
+    print('Hacking key #{}: {}' . format(key, translated))
